9cf7e785f7887701ad695167238f228b1a44557d
Nabila Zavira <nabilazavira@ui.ac.id>
2023-11-01 14:13:07 +0700

    selesai tutorial 6

--- lib/menu.dart
+++ lib/menu.dart
@@ -68,25 +68,7 @@ class MyHomePage extends StatelessWidget {
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
-
-  
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
+   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
@@ -139,4 +121,5 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+  
 }
